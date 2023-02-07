f127c41272e22d3778877d6a916f5cd1bc099cbb
QistinaFauzi <124714123+QistinaFauzi@users.noreply.github.com>
2023-02-07 17:45:19 +0000

    Add files via upload

---
+++ Web/src/main/java/com/capstone/Web/Movie.java
@@ -0,0 +1,20 @@
+package com.capstone.Web;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Movie {
+
+    @Id
+    @GeneratedValue
+    private int movieid;
+    private String title;
+    private String genres;
+    private int tmdbid;
+}
---
+++ Web/src/main/java/com/capstone/Web/Recommend.java
@@ -0,0 +1,12 @@
+package com.capstone.Web;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Recommend {
+    private int movieid;
+}
